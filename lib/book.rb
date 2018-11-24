5f8d7ba938c2229e957f6da693ecdf53fbb6ec86
Marie Burns <mcburns2222@gmail.com>
2018-11-24 19:45:24 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,23 +1,9 @@
 class Book
   attr_accessor :author, :page_count, :genre
+  attr_reader :title
+  
   def initialize(title)
     @title = title
-  end
-  
-  def title
-    @title
-  end
-  
-
-  
-
-  
-  def genre=(genre)
-    @genre = genre
-  end
-  
-  def genre
-    @genre
   end
   
   def turn_page
