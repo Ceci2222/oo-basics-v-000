10a1bddf3c4e31decafea493d6515c3736a2c8ce
Marie Burns <mcburns2222@gmail.com>
2018-11-24 19:44:12 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -8,21 +8,9 @@ class Book
     @title
   end
   
-  def author=(author)
-    @author = author
-  end
-    
-  def author
-    @author
-  end
+
   
-  def page_count=(num)
-    @page_count = num
-  end
-  
-  def page_count
-    @page_count
-  end
+
   
   def genre=(genre)
     @genre = genre
